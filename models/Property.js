bc0ecd27b393d7449dc0704bdf9d931ccb1d31a6
azim mahmud <mahamud.azim@gmail.com>
2023-10-23 08:41:27 +0600

    update property model

--- models/Property.js
+++ models/Property.js
@@ -6,33 +6,61 @@ const PropertySchema = new mongoose.Schema(
   {
     title: {
       type: String,
-      required: [true, "Please add a title"],
+      required: [true, "Please add a title."],
       unique: true,
       trim: true,
-      maxlength: [50, "Title can not be more than 50 characters"]
+      maxlength: [50, "Title can not be more than 50 characters."]
     },
     slug: String,
     description: {
       type: String,
       trim: true,
-      maxlength: [250, "Description can not be more than 250 characters"]
+      maxlength: [250, "Description can not be more than 250 characters."]
     },
     category: {
       type: String,
-      required: true,
+      required: [true, "Please select a category."],
       enum: [
-        "Rent",
-        "Sale"
+        "For Rent",
+        "For Sale"
       ]
     },
     propertyType: {
       type: String,
-      required: true,
+      required: [true, "Please select a property type."],
       enum: [
         "Apartment",
-        "House"
+        "House",
+        "Land"
       ]
     },
+    location: {
+      // GeoJSON Point
+      type: {
+        type: String,
+        enum: ["Point"]
+      },
+      coordinates: {
+        type: [Number],
+        index: "2dsphere"
+      },
+      formattedAddress: String,
+      street: String,
+      city: String,
+      state: String,
+      zipcode: String,
+      country: String
+    },
+    averageRating: {
+      type: Number,
+      min: [1, "Rating must be at least 1"],
+      max: [10, "Rating must can not be more than 10"]
+    },
+    price: Number,
+    yearlyTaxtRate: Number,
+    homeOwnerAssociationFee: Number,
+    afterPriceLabel: Number,
+    beforePriceLabel: Number,
   },
   { toJSON: { virtuals: true }, toObject: { virtuals: true } }
 );
