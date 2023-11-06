11ecf22b4b984e767d61306d85ee7b5fb22b7bac
azim mahmud <mahamud.azim@gmail.com>
2023-11-06 17:58:21 +0600

    Update route file name

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use(fileUpload());
 app.use(express.static(path.join(__dirname, "public")));
 
 // Mount Routes
-app.use("/api/v1/properties", require("./routes/property"));
+app.use("/api/v1/properties", require("./routes/properties"));
 // app.use("/api/v1/courses", require("./routes/courses"));
 
 // Error Handle
