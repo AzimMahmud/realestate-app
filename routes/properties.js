0481ee8b6dd9b13a1cd120d5a5607f5db412440c
azim mahmud <mahamud.azim@gmail.com>
2023-11-09 17:42:59 +0600

    update search function

--- routes/properties.js
+++ routes/properties.js
@@ -11,24 +11,27 @@ const {
 
 const advancedResults = require("../middleware/advancedResults");
 const Property = require("../models/Property");
+const { protect, authorize } = require('../middleware/auth');
+
 
 // Inclued other resource routers
 const router = express.Router();
 
 //Re-route into other resource routes
 
+
 router
   .route("/")
   .get(advancedResults(Property), getProperties)
-  .post(createProperty);
+  .post(protect, authorize('user', 'admin'), createProperty);
 
 router.route("/:id/photo").put(propertyPhotoUpload);
 
 router
   .route("/:id")
   .get(getProperty)
-  .put(updateProperty)
-  .delete(deleteProperty);
+  .put(protect, authorize('user', 'admin'), updateProperty)
+  .delete(protect, authorize('user', 'admin'), deleteProperty);
 
 router.route("/radius/:zipcode/:distance").get(getPropertiesWithInRadius);
 
